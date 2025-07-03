df274a55f9efdd07ad9471f71de73cb1aeabde59
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-07-03 22:05:24 +0300

    Add HTML/CSS structure for main, header and footer components

--- phone-zone/src/app/app.ts
+++ phone-zone/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./core/header/header";
+import { Footer } from "./core/footer/footer";
+import { Main } from "./core/main/main";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Header, Footer, Main],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
