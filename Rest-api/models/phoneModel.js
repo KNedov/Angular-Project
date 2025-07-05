af72c3fb54bfcdc496522b46d52901dad7cf0926
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-07-05 18:20:58 +0300

    Add displaySize and color fields
    - Extend PhoneModel schema with displaySize (number)
    - Add color field (string) with default values
    - Update model validations"

--- Rest-api/models/phoneModel.js
+++ Rest-api/models/phoneModel.js
@@ -1,32 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema.Types;
 
-const phoneSchema = new mongoose.Schema({
-    phoneName: {
-        type: String,
-        required: true
-    }, price: {
-        type: Number,
-        required: true,
-        min: 0
+const phoneSchema = new mongoose.Schema(
+    {
+        phoneName: {
+            type: String,
+            required: true,
+        },
+        displaySize: {
+            type: String,
+            required: true,
+            min: 1,
+        },
+        color: {
+            type: String,
+            required: true,
+            minlength: 1,
+            
+        },
+        price: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+        image: {
+            type: String,
+            required: true,
+            match: /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|webp))$/i,
+        },
+        buyers: [
+            {
+                type: ObjectId,
+                ref: "User",
+            },
+        ],
+        userId: {
+            type: ObjectId,
+            ref: "User",
+        },
+        comments: [
+            {
+                type: ObjectId,
+                ref: "Comment",
+            },
+        ],
     },
-    image:{
-        type: String,
-        required: true,
-        match: /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|webp))$/i
-    },
-    buyers: [{
-        type: ObjectId,
-        ref: "User"
-    }],
-    userId: {
-        type: ObjectId,
-        ref: "User"
-    },
-    comments: [{
-        type: ObjectId,
-        ref: "Comment"
-    }],
-}, { timestamps: { createdAt: 'created_at' } });
+    { timestamps: { createdAt: "created_at" } }
+);
 
-module.exports = mongoose.model('Phone', phoneSchema);
+module.exports = mongoose.model("Phone", phoneSchema);
