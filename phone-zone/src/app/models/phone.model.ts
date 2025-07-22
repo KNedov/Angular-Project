3d4192a712237e4e3e08fde728df8a4b76cc72fa
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-07-22 19:42:45 +0300

    Add Phone Model

---
+++ phone-zone/src/app/models/phone.model.ts
@@ -0,0 +1,14 @@
+import { User } from '.';
+import { Comment } from '.';
+
+export interface Phone {
+  _id: string;
+  phoneName: string;
+  displaySize: string;
+  color: string;
+  price: number;
+  image: string;
+  userId: User; // User ID of the phone owner
+  comments: Comment[]; // Array of comments associated with the phone
+  created_at: Date; // Creation date of the phone entry
+}
