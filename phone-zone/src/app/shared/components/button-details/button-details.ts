6cf76a37bf73d7a4e9e411d5e554b67a7a6a8519
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-07-25 22:11:07 +0300

    Make button-details component dynamic with phoneId input

--- phone-zone/src/app/shared/components/button-details/button-details.ts
+++ phone-zone/src/app/shared/components/button-details/button-details.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon'
+import { RouterLink } from '@angular/router';
+import { Phone } from '../../../models';
+
+
 
 @Component({
   selector: 'app-button-details',
-  imports: [MatIconModule],
+  imports: [MatIconModule,RouterLink],
   templateUrl: './button-details.html',
   styleUrl: './button-details.css'
 })
 export class ButtonDetails {
 
+  
+  @Input() phoneId!:string
 }
