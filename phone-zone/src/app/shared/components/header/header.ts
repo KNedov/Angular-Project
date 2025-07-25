49f242de37bb125b84d2a34764ee0f2daca5720c
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-07-25 19:03:40 +0300

    add Active Link DIrective

--- phone-zone/src/app/shared/components/header/header.ts
+++ phone-zone/src/app/shared/components/header/header.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon'
+import { ActiveLinkDirective } from '../../directives/active-link/active-link';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink,MatIconModule],
+  imports: [RouterLink,MatIconModule,ActiveLinkDirective],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
 })
+
+
 export class Header {
+
 
 }
