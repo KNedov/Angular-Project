ac3ab8d8cd3f6139adfdac2d67ae72565b91cc75
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-07-26 10:32:32 +0300

    Add Details feature component

---
+++ phone-zone/src/app/features/details/details.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-details',
+  imports: [],
+  templateUrl: './details.html',
+  styleUrl: './details.css'
+})
+export class Details {
+  
+
+
+
+}
