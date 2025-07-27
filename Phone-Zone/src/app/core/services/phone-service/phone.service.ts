ca648b309c520eb800530efbc263d903211e96ef
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-07-27 16:04:14 +0300

    Add PhoneService

---
+++ Phone-Zone/src/app/core/services/phone-service/phone.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Phone } from '../../../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhoneService {
+  private apiUrl="http://localhost:3000/api/phones?limit=${0}"
+  private getById="http://localhost:3000/api/phones?${id}"
+
+  constructor(private HttpClient:HttpClient){}
+
+  getPhones(limit:number=3):Observable<Phone[]>{
+    return this.HttpClient.get<Phone[]>(this.apiUrl.replace('${0}',limit.toString()))
+
+  }
+
+
+  
+}
