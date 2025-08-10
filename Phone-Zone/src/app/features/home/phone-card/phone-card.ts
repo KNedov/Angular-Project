5ea9ee36b7de1fdba740c805b14b42b3e0ab892c
Krasimir <141431815+KNedov@users.noreply.github.com>
2025-08-10 17:16:22 +0300

    Show latest comment only for logged-in users
    Wrapped the latest comment display in phone-card to only show when the user is logged in, using AuthService.

--- Phone-Zone/src/app/features/home/phone-card/phone-card.ts
+++ Phone-Zone/src/app/features/home/phone-card/phone-card.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, Signal } from '@angular/core';
 import { Comment, Phone } from '../../../models';
 import { MatIconModule } from '@angular/material/icon';
 import { ButtonDetails } from "../../../shared/components/button-details/button-details";
 import { LastCommentPipe } from '../../../shared/pipes/last-comment/last-comment.pipe';
+import { AuthService } from '../../../core/services';
 
 
 @Component({
@@ -12,5 +13,8 @@ import { LastCommentPipe } from '../../../shared/pipes/last-comment/last-comment
   styleUrl: './phone-card.css'
 })
 export class PhoneCard {
+  authService=inject(AuthService)
 @Input()phone!:Phone
+readonly isLoggedIn$: Signal<boolean> = this.authService.isLoggedIn$;
+  
 }
